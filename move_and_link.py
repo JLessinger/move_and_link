e0ad50fafe54f3895805fd81197291e773480e65
Jonathan Lessinger <jonathan.lessinger@gmail.com>
2016-07-31 23:40:49 -0400

    implement inverse, dont delete any directories

--- move_and_link.py
+++ move_and_link.py
@@ -1,10 +1,29 @@
+import os
+
+import shutil
+
+
 def default(args):
-    assert (not args.as_typed) or (args.replicate)
+    assert (not hasattr(args, 'as_typed')) or hasattr(args, 'replicate')
+    assert args.source and args.destination
 
 def inverse(args):
-    assert not (args.as_typed or args.replicate)
+    assert not hasattr(args, 'as_typed') and not hasattr(args, 'replicate')
+    assert hasattr(args, 'source') and not hasattr(args, 'destination')
+
+    src = os.path.abspath(args.source)
+    if not os.path.islink(src):
+        raise ValueError('cannot invert source that is not a symlink')
+    dst = os.path.abspath(os.readlink(src))
+    src_base = os.path.basename(src)
+    dst_base = os.path.basename(dst)
+    if src_base != dst_base:
+        raise ValueError('Link name different from target. Was this linked properly?')
+
+    os.remove(src)
+    src_par = os.path.dirname(src)
+    shutil.move(dst, src_par)
+
 
 if __name__ == '__main__':
-    # produce parsed argument object called args
-    # func should be either "default" or "inverse" depending on which subcommand is given
     args.func(args)
