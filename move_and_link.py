b59269f10a7b8934cd70ccdcbac703555d777fae
Jonathan Lessinger <jonathan.lessinger@gmail.com>
2016-08-01 00:17:24 -0400

    default seems to work, no replicate yet

--- move_and_link.py
+++ move_and_link.py
@@ -4,11 +4,32 @@ import errno
 
 
 def default(args):
-    assert (not hasattr(args, 'as_typed')) or hasattr(args, 'replicate')
+    assert (not args.as_typed) or args.replicate
     assert args.source and args.destination
 
+    src = args.source
+    if os.path.islink(src):
+        raise ValueError('move-and-link not defined if source is already a link')
+    if not (os.path.isfile(src) or os.path.isdir(src)):
+        raise ValueError('no such file or directory: {0}'.format(src))
+
+    dst = args.destination
+
+    if args.replicate:
+        if args.as_typed:
+            pass #TODO
+        else:
+            pass #TODO
+
+    mkdir_p(dst)
+    shutil.move(src, dst)
+
+    item = os.path.basename(src)
+    dst_path = os.path.join(dst, item)
+    os.symlink(os.path.abspath(dst_path), src)
+
 def inverse(args):
-    assert not hasattr(args, 'as_typed') and not hasattr(args, 'replicate')
+    assert not args.as_typed and not args.replicate
     assert hasattr(args, 'source') and not hasattr(args, 'destination')
 
     src = os.path.abspath(args.source)
@@ -26,6 +47,15 @@ def inverse(args):
 
     remove_empty_dirs(os.path.dirname(dst))
 
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
 def remove_empty_dirs(dir_path):
     """
     Deletes empty directory chain as far up as possible.
@@ -42,6 +72,11 @@ def remove_empty_dirs(dir_path):
             raise
 
 
-
 if __name__ == '__main__':
-    args.func(args)
+    args = lambda l: 1
+    args.source = 'f'
+    args.as_typed = False
+    args.replicate = False
+    #inverse(args)
+    args.destination = 'some/dir'
+    default(args)
