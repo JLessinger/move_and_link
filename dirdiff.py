4fb33acec4487a6a92200387c833999ca840a406
Jonathan Lessinger <jonathan.lessinger@gmail.com>
2016-08-20 05:56:06 -0400

    a lot

--- dirdiff.py
+++ dirdiff.py
@@ -1,13 +1,12 @@
+import collections
 from difflib import ndiff
 from utility.objects import SmartHashable
 
 
 class File(SmartHashable):
-    def __init__(self, abs_path):
-        self.abs_path = abs_path
-
-    def __eq__(self, other):
-        return self.abs_path == other.abs_path
+    def __init__(self, name):
+        super(File, self).__init__()
+        self._name = name
 
     @staticmethod
     def is_zero(diffs):
@@ -16,59 +15,94 @@ class File(SmartHashable):
         :param diffs: (possibly nested) list, tuple or set representing diff of file metadata/contents
         :return: True IFF the diff represents 0
         """
-        if isinstance(diffs, set) or isinstance(diffs, list) or isinstance(diffs, tuple):
-            return all([File.is_zero(e) for e in diffs])
-        return False
+        if isinstance(diffs, str):
+            return len(diffs) == 0
+        if isinstance(diffs, collections.Container):
+            for d in diffs:
+                if d == diffs:
+                    raise ValueError('cannot check against 0: non-str self-containing value: {0}'.format(d))
+                else:
+                    if not File.is_zero(d):
+                        return False
+        return True
 
+    @staticmethod
+    def str_diff(s1, s2):
+        fs1 = frozenset([repr(s1)])
+        fs2 = frozenset([repr(s2)])
+        return (fs1 - fs2, fs2 - fs1)
 
 class RegularFile(File):
-    def __init__(self, abs_path, contents):
-        super(RegularFile, self).__init__(abs_path)
-        self.contents = contents
+    def __init__(self, name, contents, encoding='utf-8'):
+        super(RegularFile, self).__init__(name)
+        self._contents = contents
+        self._encoding = encoding
 
     def __sub__(self, other):
-        c1, c2 = self.contents, other.contents
+        c1, c2 = self._contents, other._contents
         raw_diff = [l for l in ndiff(c1.splitlines(), c2.splitlines())]
         plus = [l[2:] for l in raw_diff if l[0] == '-']
         minus = [l[2:] for l in raw_diff if l[0] == '+']
-        return (plus, minus)
+        return (File.str_diff(self._name, other._name), (plus, minus))
+
+    def __repr__(self):
+        return repr(self.__str__())
+
+    def __str__(self):
+        return """{0} : \"{1}\"""".format(self._name, self._contents)
 
 class Directory(File):
-    def __init__(self, abs_path, reg_files, dirs, symlinks):
-        super(Directory, self).__init__(abs_path)
-        self.reg_files = reg_files
-        self.dirs = dirs
-        self.symlinks = symlinks
+    def __init__(self, name, reg_files=[], dirs=[], symlinks=[]):
+        """
+        :param name:
+        :param reg_files:
+        :param dirs:
+        :param symlinks:
+        """
+        super(Directory, self).__init__(name)
+        self._reg_files = frozenset(reg_files)
+        self._dirs = frozenset(dirs)
+        self._symlinks = frozenset(symlinks)
 
     def __sub__(self, other):
-        pass
+        return (File.str_diff(self._name, other._name),
+                (self._reg_files - other._reg_files, other._reg_files - self._reg_files),
+                (self._dirs - other._dirs, other._dirs - self._dirs),
+                (self._symlinks - other._symlinks, other._reg_files - self._reg_files))
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return self._name + \
+            '\nreg_files: ' + '\n'.join(self._reg_files) + \
+            '\nsymlinks:  ' + '\n'.join(self._symlinks) + \
+            '\nsub dirs:  ' + '\n'.join(self._dirs)
 
 class SymLink(File):
-    def __init__(self, abs_path, target_abs_path):
-        super(SymLink, self).__init__(abs_path)
-        self.target_abs_path = target_abs_path
+    def __init__(self, name, target_path):
+        super(SymLink, self).__init__(name)
+        self._target_name = target_path
 
     def __sub__(self, other):
-        if other.abs_path == self.abs_path:
-            path_diff = ([], [])
-        else:
-            path_diff = (self.abs_path, other.abs_path)
-        if other.target_abs_path == self.target_abs_path:
-            target_path_diff = ([], [])
-        else:
-            target_path_diff = (self.target_abs_path,
-                                other.target_abs_path)
-        return transpose_tuple((path_diff, target_path_diff))
+        return (File.str_diff(self._name, other._name),
+                File.str_diff(self._target_name, other._target_name))
+
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return '{0} -> {1}'.format(self._name, self._target_name)
 
 def transpose_tuple(t):
     return tuple(zip(*t))
 
 if __name__ == '__main__':
-    # f1 = RegularFile('f1', 'adc\ny')
-    # f2 = RegularFile('f2', 'adc\nx')
-    # d = f1 - f2
-    # print d
-    # print File.is_zero(d)
-    syma = SymLink('pa1', 'pa2')
-    symb = SymLink('pb1', 'pa2')
-    print symb - syma
+    f1 = RegularFile('f1', "a\nb")
+    f2 = RegularFile('f1', "a\nb")
+    d1 = Directory('d1', reg_files=[f1])
+    d2 = Directory('d2', reg_files=[f2])
+    print d1 - d2
+    print File.is_zero(d1 - d2)
+    print d1 == d2
--- move_and_link_tests.py
+++ move_and_link_tests.py
@@ -2,8 +2,6 @@ import os
 import errno
 import shutil
 import unittest
-from filecmp_r import dircmp_r
-
 
 
 def mkdir_p(path):
@@ -15,7 +13,7 @@ def mkdir_p(path):
         else:
             raise
 
-def touch(f_path, contents=None):
+def touch(f_path, contents):
     d = os.path.dirname(f_path)
     if not os.path.isdir(d):
         mkdir_p(d)
@@ -25,7 +23,6 @@ def touch(f_path, contents=None):
 
 class MainTests(unittest.TestCase):
     rel_test_root = os.path.join(os.getcwd(), 'testdir')
-    #mkdir_p()
 
     def setUp(self):
         if os.path.isdir(MainTests.rel_test_root):
@@ -33,28 +30,20 @@ class MainTests(unittest.TestCase):
         MainTests.mkdir_p_for_test('disk1')
         MainTests.mkdir_p_for_test('disk2')
 
-
     def tearDown(self):
         shutil.rmtree(MainTests.rel_test_root)
 
-    @staticmethod
-    def dircmp_for_test(rel1, rel2):
-        return dircmp_r(os.path.join(MainTests.rel_test_root, rel1),
-                      os.path.join(MainTests.rel_test_root, rel2))
     @staticmethod
     def mkdir_p_for_test(d):
         mkdir_p(os.path.join(MainTests.rel_test_root, d))
 
     @staticmethod
-    def touch_for_test(f_path):
-        touch(os.path.join(MainTests.rel_test_root, f_path))
+    def touch_for_test(f_path, contents=None):
+        touch(os.path.join(MainTests.rel_test_root, f_path), contents)
 
     def test_simple(self):
         MainTests.touch_for_test('disk1/dir/f')
-        MainTests.touch_for_test('disk2/dir')
-        dcmp = MainTests.dircmp_for_test('disk1', 'disk2')
-        print dcmp.left_only, dcmp.right_only
-        print dcmp.left, dcmp.right
+        MainTests.mkdir_p_for_test('disk2/dir')
         self.assertTrue(False)
 
 if __name__ == '__main__':
